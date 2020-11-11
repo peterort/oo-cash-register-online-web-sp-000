51e141006547550c0f1d5118d87e14fa0b533a6f
Peter Ort <peter@peterort.com>
2020-11-11 17:53:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,13 +7,19 @@ class CashRegister
     @items = []
   end
 
-  def add_item(title, price, quantity = 1)
-    @title = title
+  def add_item(item, price, quantity = 1)
     @price = price
     @quantity = quantity
-    @currenttotal = @price * @quantity
-    @total = @total + @currenttotal
-    @items << title
+    @total += @price * @quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter +=1
+      end
+    else
+      @items << item
+    end
   end
 
   def apply_discount
