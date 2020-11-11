f0d3fb223ac6ae7c9af20e11c2a066c4cfe7241a
Peter Ort <peter@peterort.com>
2020-11-11 18:01:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,42 +8,24 @@ class CashRegister
     @last_transaction = []
   end
 
-  def add_item(item, price, quantity = 1)
-    @price = price
-    @quantity = quantity
-    @total += @price * @quantity
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter +=1
-      end
-    else
-      @items << item
-    end
-  end
-
-  def apply_discount
-    if @discount > 0
-    @discount_price = (@total * @discount)/ 100
-    @total -= @discount_price
-    return "After the discount, the total comes to $#{@total}."
-  else
-    return "There is no discount to apply."
-  end
-  end
-
-  def items
-    @items
-  end
-
-def void_last_transaction
-  @last = @last_transaction.pop
-  @total - @last
-
+  def add_item(item_name, price, quantity = 1)
+  @total += quantity * price
+  quantity.times { @items.push(item_name) }
+  @last_transaction.push([item_name, price, quantity])
 end
 
+def apply_discount
+  if discount == 0
+    "There is no discount to apply."
+  else
+    @total -= (@total *(@discount / 100.0)).to_i
+    "After the discount, the total comes to $#{@total}."
+  end
+end
 
-
+def void_last_transaction
+  last_transaction = @last_transaction.pop
+  @total -= last_transaction[1] * last_transaction[2]
+end
 
 end
